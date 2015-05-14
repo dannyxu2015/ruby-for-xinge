8ed917df110a560807b9b97ed6b40b6017bfff74
xh <xh223@dfc.cn>
2015-05-14 12:42:56 +0800

    can specify send time
    指定推送时间，格式为year-mon-day hour:min:sec
    若小于服务器当前时间，则会立即推送（参考腾讯信鸽后台RESTFUL开发文档）

--- lib/xinge/android.rb
+++ lib/xinge/android.rb
@@ -5,20 +5,21 @@ module Xinge
       super
     end
 
-    def push_to_single_device(token, title, content, params={}, custom_content={})
-      self.push_single_device(token, 2, build_simple_message(title, content, custom_content), params)
+    # send_time: 指定推送时间，格式为year-mon-day hour:min:sec 若小于服务器当前时间，则会立即推送
+    def push_to_single_device(token, title, content, params={}, custom_content={}, send_time='')
+      self.push_single_device(token, 2, build_simple_message(title, content, custom_content), params.merge({send_time: send_time}))
     end
 
-    def push_to_all_device(title, content, params={}, custom_content={})
-      self.push_all_device(2, build_simple_message(title, content, custom_content), params)
+    def push_to_all_device(title, content, params={}, custom_content={}, send_time='')
+      self.push_all_device(2, build_simple_message(title, content, custom_content), params.merge({send_time: send_time}))
     end
 
-    def push_to_single_account(account, title, content, params={}, custom_content={})
-      self.push_single_account(account, 2, build_simple_message(title, content, custom_content), params)
+    def push_to_single_account(account, title, content, params={}, custom_content={}, send_time='')
+      self.push_single_account(account, 2, build_simple_message(title, content, custom_content), params.merge({send_time: send_time}))
     end
 
-    def push_to_tags_device(title, content, tags, params={}, custom_content={}, tag_op='OR')
-      self.push_tags_device(2, build_simple_message(title, content, custom_content), tags, tag_op, params)
+    def push_to_tags_device(title, content, tags, params={}, custom_content={}, tag_op='OR', send_time='')
+      self.push_tags_device(2, build_simple_message(title, content, custom_content), tags, tag_op, params.merge({send_time: send_time}))
     end
 
     protected
